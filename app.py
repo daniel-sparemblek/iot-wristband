65e87963dd84def1a36d814a5965a6605558a710
Daniel <danielrey1@gmail.com>
2021-02-24 18:53:20 +0100

    aaaa

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
+books = [{'name' : 'Snow White', 'author' : 'Grimm brothers'},
+         {'name' : "Alice's advenures in Wonderland", 'author' : 'Lewis Carrol'}
+]
 
-@app.route('/')
+@app.route("/", methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
+
+@app.route("api/books", methods=['GET'])
+def return_all():
+    return jsonify({'books' : books})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
